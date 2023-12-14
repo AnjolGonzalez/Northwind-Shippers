2ec718e93e8d887868231cfdef3b987e245f15c4
Angel Gonzalez <agonzalez000@appdev.yearup.org>
2023-12-14 13:20:42 -0500

    Finished

--- src/main/java/com/pluralsight/DataManager.java
+++ src/main/java/com/pluralsight/DataManager.java
@@ -31,30 +31,28 @@ public class DataManager {
                 Please enter the following information:
                 Name and Phone number...
                 """);
-        String input = String.valueOf(scanner.nextLine().trim().equalsIgnoreCase(""));
+        String input = scanner.nextLine().trim();
         String[] input2 = input.split(" ");
 
         try (
                 Connection connection = dataSource.getConnection();
-
                 PreparedStatement preparedStatement = connection.prepareStatement(query);
-
                 PreparedStatement statement = connection.prepareStatement(query2)) {
 
-                    statement.setString(1, input2[0]);
-                    statement.setString(2, input2[0]);
-                    int values = statement.executeUpdate();
+            statement.setString(1, input2[0]);
+            statement.setString(2, input2[1]);
 
-                    try (ResultSet results = preparedStatement.executeQuery()) {
-                        while (results.next()) {
-                            System.out.println("ID: " + results.getString("ShipperID"));
-                            System.out.println("CompanyName: " + results.getString("CompanyName"));
-                            System.out.println("Phone: " + results.getString("Phone"));
-                            System.out.println("---------------------------");
-                        }
-                    }
+            try (ResultSet results = preparedStatement.executeQuery()) {
+                while (results.next()) {
+                    System.out.println("ID: " + results.getString("ShipperID"));
+                    System.out.println("CompanyName: " + results.getString("CompanyName"));
+                    System.out.println("Phone: " + results.getString("Phone"));
+                    System.out.println("---------------------------");
+                }
+            }
+            scanner.close();
 
-        }catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
 
@@ -65,13 +63,13 @@ public class DataManager {
                 Welcome,
                 Would you like to continue and update your phone number? (yes or no)
                 """);
-        String choice = String.valueOf(scanner.nextLine().trim().equalsIgnoreCase(" "));
+        String choice = scanner.nextLine().trim();
 
         if (choice.trim().equalsIgnoreCase("Yes")) {
             try (
                     Connection connection = dataSource.getConnection();
                     PreparedStatement statement = connection.prepareStatement(query);
-                    ) {
+            ) {
                 try (ResultSet results = statement.executeQuery()) {
                     while (results.next()) {
                         System.out.println("ID: " + results.getString("ShipperID"));
@@ -81,16 +79,85 @@ public class DataManager {
                     }
                 }
                 System.out.println("Would you like to change your information? If so, please enter the id and phone number of shipper information.");
+                String input = scanner.nextLine();
+                String[] choiceSplit = input.split(" ");
+                String query = "UPDATE Shippers SET Phone = ? WHERE ShipperID = ? AND Phone = ?";
+                System.out.println("Please enter new number");
+                String userNewNumber = String.valueOf(scanner.nextLine().trim().equalsIgnoreCase(" "));
+
+                try (
+                        PreparedStatement statedQuery = connection.prepareStatement(query)
+                ) {
+                    statedQuery.setString(1, userNewNumber);
+                    statedQuery.setString(2, choiceSplit[0]);
+                    statedQuery.setString(3, choiceSplit[1]);
+                    int infoUpdated = statedQuery.executeUpdate();
+
+                    if (infoUpdated > 0) {
+                        System.out.println("Updated information");
+                    } else {
+                        System.out.println("Please try again");
+                    }
+                    try (ResultSet results = statement.executeQuery()) {
+                        while (results.next()) {
+                            System.out.println("ID: " + results.getString("ShipperID"));
+                            System.out.println("CompanyName: " + results.getString("CompanyName"));
+                            System.out.println("Phone: " + results.getString("Phone"));
+                            System.out.println("---------------------------");
+                        }
+                    }
+                }
+                scanner.close();
 
             } catch (SQLException e) {
                 e.printStackTrace();
             }
 
-        }else {
+        } else {
             System.out.println("Have a nice day!");
             System.exit(0);
         }
     }
 
+    public static void deleteShipper() {
+        try (
+                Connection connection = dataSource.getConnection();
+                PreparedStatement statement = connection.prepareStatement(query)) {
+            try (ResultSet results = statement.executeQuery()) {
+                while (results.next()) {
+                    System.out.println("ID: " + results.getString("ShipperID"));
+                    System.out.println("CompanyName: " + results.getString("CompanyName"));
+                    System.out.println("Phone: " + results.getString("Phone"));
+                    System.out.println("---------------------------");
+                }
+            }
+            System.out.println("Please enter the ID of the shipper you would like to delete.");
+            String input = scanner.nextLine().trim();
 
+            try (
+                    PreparedStatement delete = connection.prepareStatement("DELETE FROM Shippers WHERE ShipperID = ?")
+            ){
+                delete.setString(1, input);
+                int infoDeleted = delete.executeUpdate();
+                if (infoDeleted > 0) {
+                    try (ResultSet results = statement.executeQuery()) {
+                        while (results.next()) {
+                            System.out.println("ID: " + results.getString("ShipperID"));
+                            System.out.println("CompanyName: " + results.getString("CompanyName"));
+                            System.out.println("Phone: " + results.getString("Phone"));
+                            System.out.println("---------------------------");
+                        }
+                        System.out.println("Here is your updated data");
+                        System.out.println("Successful");
+                    }
+                } else {
+                    System.out.println("Please try again");
+                }
+                scanner.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
--- src/main/java/com/pluralsight/mainApp.java
+++ src/main/java/com/pluralsight/mainApp.java
@@ -1,10 +1,33 @@
 package com.pluralsight;
+import static com.pluralsight.DataManager.*;
+
+import java.util.Scanner;
 
 public class mainApp {
     public static void main(String[] args) {
-
         DataManager.setConnection();
-        DataManager.shipper();
+        System.out.println("""
+                Welcome,
+                What would you like to do?
+                1) Add a new shipper
+                2) Update shipper information
+                3) Delete shipper information
+                0 Exit
+                """);
+        String choice = scanner.nextLine();
+        switch (choice) {
 
+            case "1":
+                DataManager.shipper();
+                break;
+            case "2":
+                DataManager.updateShipper();
+                break;
+            case "3":
+                DataManager.deleteShipper();
+                break;
+            case "0":
+                System.exit(0);
+        }
     }
 }
